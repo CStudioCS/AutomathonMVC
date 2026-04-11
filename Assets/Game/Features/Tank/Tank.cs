74f8387efc3503a75ac1b3e8c2827e01f26ea80a
Fypur <84095553+Fypur@users.noreply.github.com>
2026-04-11 23:58:29 +0200

    Basic input providers

---
+++ Assets/Game/Features/Tank/Tank.cs
@@ -0,0 +1,21 @@
+﻿using Automathon.Engine;
+using Automathon.Engine.Physics;
+
+namespace Automathon.Game.Tank
+{
+    public class Tank : Entity
+    {
+        public Tank(Vector2Int position) : base(position)
+        {
+            AddComponent(new BoxCollider(Vector2Int.Zero, 20, 20,  0));
+            AddComponent(new Rigidbody());
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            
+        }
+    }
+}
