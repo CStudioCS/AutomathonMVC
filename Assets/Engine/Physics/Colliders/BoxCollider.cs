53572665720a061aa2c0ff51cdf4eff1b3b145f8
fares <amine.gati311@gmail.com>
2026-04-11 18:35:19 +0200

    BoxCollider and CircleCollider

---
+++ Assets/Engine/Physics/Colliders/BoxCollider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automathon.Engine.Physics
+{
+    public class BoxCollider : Collider
+    {
+        public Vector2Int LocalPosition;
+        public int Width;
+        public int Height;
+        public int RotationMillirad;
+
+        public BoxCollider(Vector2Int localPosition, int width, int height, int rotationMillirad)
+        {
+            LocalPosition = localPosition;
+            Width = width;
+            Height = height;
+            RotationMillirad = rotationMillirad;
+        }
+
+        public override bool Colliding(Collider collider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
