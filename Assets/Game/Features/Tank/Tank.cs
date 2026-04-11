3d2ebbca9f5bf5b55b173188165532bcf39c1109
Fypur <84095553+Fypur@users.noreply.github.com>
2026-04-12 00:01:24 +0200

    Tank base

--- Assets/Game/Features/Tank/Tank.cs
+++ Assets/Game/Features/Tank/Tank.cs
@@ -1,21 +1,28 @@
 ﻿using Automathon.Engine;
 using Automathon.Engine.Physics;
+using Automathon.Game.Input;
 
 namespace Automathon.Game.Tank
 {
     public class Tank : Entity
     {
-        public Tank(Vector2Int position) : base(position)
+        private const int speed = 5;
+
+        private IInputProvider inputProvider;
+        private Rigidbody rb;
+
+        public Tank(Vector2Int position, IInputProvider inputProvider) : base(position)
         {
+            this.inputProvider = inputProvider;
             AddComponent(new BoxCollider(Vector2Int.Zero, 20, 20,  0));
-            AddComponent(new Rigidbody());
+            rb = AddComponent(new Rigidbody());
         }
 
         public override void Update()
         {
             base.Update();
 
-            
+            rb.Velocity = inputProvider.MilliMovementDir() * speed / 1000;
         }
     }
 }
