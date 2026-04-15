757dbdd536699af4cf44950a30cbee8adfff2276
pro2p <pierre.fessart@gmail.com>
2026-04-15 21:33:58 +0200

    weapon system implemented
    ability class allows for reload of abilities and activation of them, basic shield system using ability class.

---
+++ Assets/Game/Features/Ability/Ability.cs
@@ -0,0 +1,41 @@
+using Automathon.Engine;
+using System;
+
+
+namespace Automathon.Game.AbilitySystem
+{
+    public abstract class Ability : Component
+    {
+        private int coolDownMillis;
+        private bool isOnCooldown = false;
+        private Func<bool> shouldActivate;
+
+        public Ability(int cooldown, Func<bool> shouldActivate)
+        {
+            this.coolDownMillis = cooldown;
+            this.shouldActivate = shouldActivate;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if(shouldActivate())
+            {
+                TryActivate();
+            }
+        }
+
+        public void TryActivate()
+        {
+            if (isOnCooldown)
+                return;
+
+            isOnCooldown = true;
+            ParentEntity.AddBehavior(new Timer(coolDownMillis, null, OnComplete: () => isOnCooldown = false));
+            Activate();
+        }
+
+        protected abstract void Activate();
+    }
+}
