e19c994c5cf8bed9416041f1014068256b57489c
mathisramalho-source <mathis.ramalho@gmail.com>
2026-04-15 22:49:19 +0200

    Update WallView.cs

--- Assets/Game/Features/Wall/WallView.cs
+++ Assets/Game/Features/Wall/WallView.cs
@@ -13,10 +13,6 @@ namespace Automathon.Game.WallSystem
             this.wall = wall;
             transform.localScale = new Vector3(wall.collider.Width / WorldConstants.SPACE_SCALE, wall.collider.Height / WorldConstants.SPACE_SCALE, 1);
         }
-
-
-
     }
-
 }
 
