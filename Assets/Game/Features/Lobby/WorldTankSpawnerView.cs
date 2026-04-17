604923644362cd221e971ca02656587a18c5229a
Cedric Vilandrau <cedric.vilandrau@gmail.com>
2026-04-17 23:08:57 +0200

    1

--- Assets/Game/Features/Lobby/WorldTankSpawnerView.cs
+++ Assets/Game/Features/Lobby/WorldTankSpawnerView.cs
@@ -13,11 +13,12 @@ namespace Automathon.Game.Lobby
         [Header("Setup")]
         [SerializeField] private PlayerInputManager playerInputManager;
         [SerializeField] private TankView tankViewPrefab;
+        [SerializeField] private GameObject tankPrefab;
         public event Action<PlayerInput> PlayerJoined;
 
         List<PlayerInput> players = new();
         public static WorldTankSpawnerView Instance { get; private set; }
-        private bool hasKeyboardPlayerJoined = false;
+        private bool hasGamepadPlayerConnected = false;
         private bool hasKeyboardPlayerConnected = false;
         public void Awake()
         {
@@ -87,18 +88,21 @@ namespace Automathon.Game.Lobby
                 }
             }
 
-            string manufacturer = gamepad.device.description.manufacturer.ToLower();
-            string deviceName = gamepad.displayName.ToLower();
-            string description = gamepad.description.product.ToLower();
-
             // Join using the Gamepad device. 
             // Leave controlScheme null or set to "Gamepad" if you have a specific scheme named that.
 
-            playerInputManager.JoinPlayer(
+            PlayerInput playerInput = playerInputManager.JoinPlayer(
             playerIndex: -1,
             splitScreenIndex: -1,
             controlScheme: "Gamepad",
             pairWithDevice: gamepad);
+
+            TankView tankView = playerInput.GetComponent<TankView>();
+            tankView.PlayerInput = playerInput;
+            Tank tank = new Tank(new Automathon.Vector2Int(0, 0), new PlayerInputProvider(tankView.PlayerInput));
+
+            GameplayManager.Instance.Instantiate(tank);
+            tankView.Initialize(tank);
         }
         private void JoinKeyboardPlayer()
         {
@@ -110,34 +114,34 @@ namespace Automathon.Game.Lobby
                 return;
             }
 
-            if (hasKeyboardPlayerConnected)
-                return;
-            hasKeyboardPlayerConnected = true;
+            foreach(PlayerInput playerInputTemp in PlayerInput.all)
+            {
+                if (playerInputTemp.currentControlScheme == "Keyboard&Mouse")
+                    return;
+            }
             // 2. Manually trigger the join
             // We pass -1 for playerIndex to let Unity assign the next available index (0, 1, 2, etc.)
-            Debug.Log("Joining keyboard player");
-            playerInputManager.JoinPlayer(
-                playerIndex: -1,
+            PlayerInput playerInput = playerInputManager.JoinPlayer(
+                playerIndex: 1,
                 splitScreenIndex: -1,
                 controlScheme: "Keyboard&Mouse",
                 pairWithDevice: Keyboard.current
             );
+
+            TankView tankView = playerInput.GetComponent<TankView>();
+            tankView.PlayerInput = playerInput;
+            Tank tank = new Tank(new Automathon.Vector2Int(0, 0), new PlayerInputProvider(tankView.PlayerInput));
+
+            GameplayManager.Instance.Instantiate(tank);
+            tankView.Initialize(tank);
         }
         public void OnPlayerJoined(PlayerInput playerInput)
         {
             if (playerInput == null) return;
 
-            if (hasKeyboardPlayerJoined)
-                return;
-            hasKeyboardPlayerJoined = true;
-
             playerInput.transform.position = new Vector3(0, 0, 0);
 
             players.Add(playerInput);
-            TankView tankView = Instantiate(tankViewPrefab);
-            Tank tank = new Tank(new Automathon.Vector2Int(0, 0), new PlayerInputProvider(tankView.PlayerInput));
-            GameplayManager.Instance.Instantiate(tank);
-            tankView.Initialize(tank);
         }
         public void OnPlayerLeft(PlayerInput playerInput)
         {
