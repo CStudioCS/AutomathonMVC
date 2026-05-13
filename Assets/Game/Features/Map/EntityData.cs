d668e9f4b0e30e2690db350286d87b5929803ffe
mathisramalho-source <mathis.ramalho@gmail.com>
2026-05-13 23:34:39 +0200

    petit fix
    suppression fichier inutile

--- Assets/Game/Features/Map/EntityData.cs
+++
@@ -1,6 +0,0 @@
-// EntityData has moved to Assets/Engine/Entity/EntityData.cs (namespace Automathon.Engine).
-// This file is now empty and can be safely deleted from Unity's Project window
-// (along with its .meta file).
-namespace Automathon.Game.MapSystem
-{
-}
