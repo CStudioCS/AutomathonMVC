7679aa7c28fb8f6fffa5a445a3b14b514705f7dc
pro2p <pierre.fessart@gmail.com>
2026-06-11 00:23:55 +0200

    added background and tank sprites

--- Assets/Game/Features/Tank/Tank.cs
+++ Assets/Game/Features/Tank/Tank.cs
@@ -7,6 +7,8 @@ namespace Automathon.Game
     public class Tank : Entity
     {
         private const int SIZE = 600;
+        private const int TANK_HEIGHT = 838;
+        private const int TANK_WIDTH = 1138;
         private const int SPEED = 6000;
         public const int MAX_HEALTH = 1000;
 
@@ -26,7 +28,7 @@ namespace Automathon.Game
         {
             InputProvider = inputProvider;
 
-            BoxCollider boxCollider = new BoxCollider(Vector2Int.Zero, SIZE, SIZE, 0);
+            BoxCollider boxCollider = new BoxCollider(Vector2Int.Zero, TANK_WIDTH, TANK_HEIGHT, 0);
             rigidbody = new Rigidbody(boxCollider, 1000, 500, 200);
 
             Initialize(
