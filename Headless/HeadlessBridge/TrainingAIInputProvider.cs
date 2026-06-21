77d7e37352333374311e72f10718909ad6bc98c5
fares <84095553+Fypur@users.noreply.github.com>
2026-06-21 16:21:18 +0200

    Few modif

--- Headless/HeadlessBridge/TrainingAIInputProvider.cs
+++ Headless/HeadlessBridge/TrainingAIInputProvider.cs
@@ -5,8 +5,27 @@ namespace HeadlessBridge;
 
 public class TrainingAIInputProvider : IInputProvider
 {
+    private Vector2Int movingDir = new Vector2Int(1000, 0);
+    private Vector2Int aimingDir = new Vector2Int(1000, 0);
     public void UpdateFromAction(float[] actionVector)
     {
+
+
+        //Attention : Data du user donc programmer defensivement pour eviter les erreurs
+        Vector2Int move = new Vector2Int((int)(actionVector[0] * 1000), (int)(actionVector[1] * 1000));
+        if (move != Vector2Int.Zero)
+        {
+            move.NormalizeAtScale(1000);
+            movingDir = move;
+        }
+
+        Vector2Int aim = new Vector2Int((int)(actionVector[2] * 1000), (int)(actionVector[3] * 1000));
+        if (aim != Vector2Int.Zero)
+        {
+            aim.NormalizeAtScale(1000);
+            aimingDir = aim;
+        }
+
 
     }
 
--- Headless/HeadlessPython/README.md
+++ Headless/HeadlessPython/README.md
@@ -1,2 +1,13 @@
+When specifying X and Y, we're talking about vector directions. 
+
+Action vector : 
+0 - MovingDir.X
+1 - MovingDir.Y (the moving vector should have a norm under or equal 1, else it will normalized to 1, also if sent as (0,0) then it won't change)
+2 - AimingDir.X
+3 - AimingDir.Y (aiming dir is normalized to 1, if set to (0, 0) it won't change)
+4 - Shoot
+
+
+
 TO REGENERATE PROTOBUF FILES :
 python -m grpc_tools.protoc -I../.. --python_out=. --pyi_out=. --grpc_python_out=. proto.proto
