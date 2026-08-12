74164f143967bdf1079dc5e94a1bdcc7abaa9b4c
Fypur <84095553+Fypur@users.noreply.github.com>
2026-08-12 22:06:49 +0200

    Small renaming for clarification

--- PythonAI/bridge/gym.py
+++ PythonAI/bridge/gym.py
@@ -10,11 +10,11 @@ class Gym:
         self.poller = zmq.Poller()
         self.poller.register(self.socket, zmq.POLLIN)
 
-    def step(self, self_action: AIAction, enemy_action: AIAction, timeout : None | int=500):
+    def step(self, self_action: AIAction, enemy_action: AIAction, tcp_connection_timeout : None | int=500):
 
         self.__send_action__(AIMessage(Reset=False, DoneWithTraining=False, SelfAction=self_action, EnemyAction=enemy_action))
 
-        if self.poller.poll(timeout):
+        if self.poller.poll(tcp_connection_timeout):
             return self.__receive_state__()
         
         raise TimeoutError()
